98d84bcc63353c4098fe0efb225e840b223667eb
Ian Forrest <ianforrest11@gmail.com>
2019-09-22 17:30:43 -0400

    performed exploratory analysis on 2017 ACCIDENT.csv file

--- explore.py
+++ explore.py
@@ -1,1 +1,6 @@
-"""initial exploratory testing for DS portion of saferoutes project"""
+import pandas as pd
+
+acc = pd.read_csv('/Users/ianforrest/Desktop/coding/repos/DS_Safe-Routes/ACCIDENT.CSV')
+acc_loc = acc[['ST_CASE', 'latitude', 'longitud']]
+
+print(acc_loc.describe())
